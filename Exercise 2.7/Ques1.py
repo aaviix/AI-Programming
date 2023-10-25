3b069004753695eeef90c5e93588900a481f7a15
Avanish Singh <avanish.singh@stud.th-deg.de>
2023-10-25 10:16:46 +0200

    Ques1.py

--- Exercise 2.7/Ques1.py
+++ Exercise 2.7/Ques1.py
@@ -1,1 +1,10 @@
+## Author : 22200727
+## Question 1. 
+# Define a function rotateR for strings:
 
+def rotateR(s):
+    return s[-1] + s[:-1]
+    
+# Example usage:
+result1 = rotateR("Thor")
+print(result1)  # Output: "rTho"
